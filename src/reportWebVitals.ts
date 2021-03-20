dde1b2b099d6a6b804e57f8ff8800676f9472f14
kento_ootani <kento_ootani@am.sumitomolife.co.jp>
2021-03-20 19:46:12 +0900

    [add] CRAの実行とlinter, code-formatterの設定

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,18 @@
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      // eslint-disable-next-line no-console
+      .catch((err) => console.error(err));
+  }
+};
+
+export default reportWebVitals;
